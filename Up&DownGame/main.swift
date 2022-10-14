092698f6ce951a6e7d55622827952f0520afdfc2
dami0806 <dami0806>
2022-10-14 19:14:54 +0900

    bingoGame

--- Up&DownGame/main.swift
+++ Up&DownGame/main.swift
@@ -10,7 +10,11 @@ import Foundation
 var computerChoice :Int = Int.random(in: 1...100)
 var myChoice : Int = 0
 
+
+while true{
 var userInput = readLine()  //?가 있으면 optional input
+
+
 
 if let Input = userInput{  //optional ->string
    if let num = Int(Input){ // string-->Int
@@ -18,5 +22,15 @@ if let Input = userInput{  //optional ->string
         
     }
 }
-print(myChoice)
 
+//컴퓨터와 나 숫자 비교
+if computerChoice > myChoice{
+    print("up!")
+    
+    }else if computerChoice<myChoice{
+    print("down!")
+    }else {
+        print("Bingo!")
+        break
+    }
+}
